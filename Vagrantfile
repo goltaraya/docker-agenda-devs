076e1d4b9bfafcc381150f62722ca32bf6ffbc65
Yago Alexandre <yago@pop-os.localdomain>
2023-03-21 21:49:16 -0300

    Inserting and modifying docker-compose.yaml

--- Vagrantfile
+++ Vagrantfile
@@ -1,5 +1,13 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "almalinux/8"
+  
   config.vm.hostname = "docker-host"
+
+  config.vm.network "public_network", bridge: "wlp2s0"
+  config.vm.network "forwarded_port", guest: 80, host: 8080
+  config.vm.network "forwarded_port", guest: 3306, host: 3306
+  
+  config.vm.provision "file", source: "./Dockerfile", destination: "/tmp/"
+  config.vm.provision "file", source: "./docker-compose.yaml", destination: "/tmp/"
   config.vm.provision "shell", path: "provision.sh"
 end
--- provision.sh
+++ provision.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 sudo dnf install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+echo "Installing and setting up Docker"
+sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin > /dev/null 2>&1
 sudo systemctl enable docker
 sudo systemctl start docker
+
+echo "Installing and setting up Git"
+sudo dnf install -y git > /dev/null 2>&1
+
+echo "Moving Dockerfile and docker-compose.yaml from host machine to guest machine [/opt/agenda/]"
+sudo mkdir /opt/agenda/ 
+sudo cp /tmp/Dockerfile /tmp/docker-compose.yaml /opt/agenda/
